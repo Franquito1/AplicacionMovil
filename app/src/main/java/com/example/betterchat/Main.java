70df7eb8e6bc3a084e576ef33cc39f1f932f46f4
Franquito1 <juan.francov@uniagustiniana.edu.co>
2023-05-10 11:33:34 -0500

    Agregando vistas barra de navegacion

---
+++ app/src/main/java/com/example/betterchat/Main.java
@@ -0,0 +1,51 @@
+package com.example.betterchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Main extends AppCompatActivity {
+
+    UserFragment firstFragment = new UserFragment();
+    ChatsFragment secondFragment = new ChatsFragment();
+    ConfigFragment thirdFragment = new ConfigFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.PrimerFragmento:
+                    loadFragment(firstFragment);
+                    return true;
+                case R.id.SegundoFragmento:
+                    loadFragment(secondFragment);
+                    return true;
+                case R.id.TercerFragmento:
+                    loadFragment(thirdFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+    }
+}
