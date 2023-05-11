a150bcdb434be57154a59f04f2c71a2fec0219a5
Franquito1 <juan.francov@uniagustiniana.edu.co>
2023-05-10 23:28:14 -0500

    Pantalla de carga

--- app/src/main/java/com/example/betterchat/MainActivity.java
+++ app/src/main/java/com/example/betterchat/MainActivity.java
@@ -17,86 +17,28 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class    MainActivity extends AppCompatActivity{
 
-
-    EditText EmailLogin, PasswordLogin;
-    Button button_InicioSesion;
-    TextView textView_OlvidastePassword, textView_RegistraseLogin;
-    FirebaseAuth mAuth;
 
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
 
-        EmailLogin = findViewById(R.id.EditText_EmailLogin);
-        PasswordLogin = findViewById(R.id.EditText_PasswordLogin);
-        button_InicioSesion = findViewById(R.id.button_InicioSesion);
-        textView_RegistraseLogin = findViewById(R.id.textView_RegistraseLogin);
-        textView_OlvidastePassword = findViewById(R.id.textView_OlvidastePassword);
-
-
-        textView_OlvidastePassword.setOnClickListener(new View.OnClickListener() {
+        TimerTask tarea = new TimerTask() {
             @Override
-            public void onClick(View view) {
-                Intent intent1 = new Intent(MainActivity.this,recuperarPassword.class);
-                startActivity(intent1);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,loginActivity.class);
+                startActivity(intent);
                 finish();
             }
-        });
-
-        textView_RegistraseLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // crear un Intent para abrir la nueva actividad
-                Intent intent = new Intent(MainActivity.this, Activity_RegistroUsuario.class);
-                startActivity(intent);
-            }
-        });
-
-        button_InicioSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String EmailUser = EmailLogin.getText().toString().trim();
-                String PasswordUser = PasswordLogin.getText().toString().trim();
-
-             if(EmailUser.isEmpty() && PasswordUser.isEmpty()){
-
-                 Toast.makeText(MainActivity.this, "Ingresa tus datos.", Toast.LENGTH_SHORT).show();
-
-             }else{
-
-                loginUser(EmailUser,PasswordUser);
-
-             }
-            }
-
-            private void loginUser(String emailUser, String passwordUser) {
-
-                mAuth.signInWithEmailAndPassword(emailUser,passwordUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            finish();
-                            startActivity(new Intent(MainActivity.this,Main.class));
-                            Toast.makeText(MainActivity.this, "Iniciaste Sesion Correctamente.", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(MainActivity.this, "Error al Iniciar Sesion unu.", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error al Iniciar Sesion: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-            }
-        });
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,2000);
     }
 }
---
+++ app/src/main/java/com/example/betterchat/loginActivity.java
@@ -0,0 +1,101 @@
+package com.example.betterchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class  loginActivity extends AppCompatActivity{
+
+
+    EditText EmailLogin, PasswordLogin;
+    Button button_InicioSesion;
+    TextView textView_OlvidastePassword, textView_RegistraseLogin;
+    FirebaseAuth mAuth;
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        EmailLogin = findViewById(R.id.EditText_EmailLogin);
+        PasswordLogin = findViewById(R.id.EditText_PasswordLogin);
+        button_InicioSesion = findViewById(R.id.button_InicioSesion);
+        textView_RegistraseLogin = findViewById(R.id.textView_RegistraseLogin);
+        textView_OlvidastePassword = findViewById(R.id.textView_OlvidastePassword);
+
+
+        textView_OlvidastePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(loginActivity.this,recuperarPassword.class);
+                startActivity(intent1);
+                finish();
+            }
+        });
+
+        textView_RegistraseLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // crear un Intent para abrir la nueva actividad
+                Intent intent = new Intent(loginActivity.this, Activity_RegistroUsuario.class);
+                startActivity(intent);
+            }
+        });
+
+        button_InicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String EmailUser = EmailLogin.getText().toString().trim();
+                String PasswordUser = PasswordLogin.getText().toString().trim();
+
+                if(EmailUser.isEmpty() && PasswordUser.isEmpty()){
+
+                    Toast.makeText(loginActivity.this, "Ingresa tus datos.", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    loginUser(EmailUser,PasswordUser);
+
+                }
+            }
+
+            private void loginUser(String emailUser, String passwordUser) {
+
+                mAuth.signInWithEmailAndPassword(emailUser,passwordUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(loginActivity.this,Main.class));
+                            Toast.makeText(loginActivity.this, "Iniciaste Sesion Correctamente.", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(loginActivity.this, "Error al Iniciar Sesion unu.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(loginActivity.this, "Error al Iniciar Sesion: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+    }
+}
